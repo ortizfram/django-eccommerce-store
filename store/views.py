e4d6926de0e18fd8ff697b9056c40cb572da4a8e
melon <ortizfranco48@gmail.com>
2023-10-02 18:49:32 -0300

    gitignore

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, '{% url "store:home" %}', {'products':products})
