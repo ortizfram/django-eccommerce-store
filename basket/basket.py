e86cbe7e29d2a6c96f8f9c22e788614dbc4b3201
melon <ortizfranco48@gmail.com>
2023-10-03 18:51:57 -0300

    JQuery add-btn

--- basket/basket.py
+++ basket/basket.py
@@ -10,5 +10,16 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number':1231231}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price':product.price}
+        
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from store.models import Product
+
+from .basket import Basket
+from decimal import Decimal 
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
+        
+        # Convert the Decimal value to float before creating the JSON response
+        response_data = {'test': 'data', 'price': float(product.price)}
+        response = JsonResponse(response_data)
+        return response
