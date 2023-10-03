d810732720c15b73028f9e9635ac06a190ec2380
melon <ortizfranco48@gmail.com>
2023-10-03 16:52:09 -0300

    basket class file with session

--- basket/basket.py
+++ basket/basket.py
@@ -7,3 +7,8 @@ class Basket():
     """
 
     def __init__(self, request):
+        self.session = request.session
+        basket = self.session.get('skey')
+        if 'skey' not in request.session:
+            basket = self.session['skey'] = {}
+        self.basket = basket
