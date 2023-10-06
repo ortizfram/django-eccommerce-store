d928be46d93685980638c7fc73352ae623725599
ortizfram <ortizfranco48@gmail.com>
2023-10-06 09:44:33 -0300

    vhanging ui

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
